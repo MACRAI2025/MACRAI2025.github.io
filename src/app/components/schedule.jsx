bc3b02a2d6fd3813b8a4b748b78e1edca37146a8
wenzhejia <1547321945@qq.com>
2025-06-30 12:56:16 +0800

    11

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -25,47 +25,46 @@ const Schedule = () => {
           <tr className="border-t">
             <td className="px-4 py-2 whitespace-nowrap">14:35–15:00</td>
             <td className="px-4 py-2">Invited talk (Guido de Croon)</td>
-            <td className="px-4 py-2">
-              The talk is focused on deep learning-based planning and control for aerial robots
-            </td>
+            <td className="px-4 py-2"> TBD</td>
           </tr>
           <tr className="border-t bg-gray-50">
             <td className="px-4 py-2 whitespace-nowrap">15:00–15:25</td>
+            <td className="px-4 py-2">Invited talk (Guillaume Adrien Sartoretti)</td>
+            <td className="px-4 py-2">High-Dimensional Multi-Agent Robot Learning</td>
+          </tr>
+          <tr className="border-t">
+            <td className="px-4 py-2 whitespace-nowrap">15:25–16:05</td>
             <td className="px-4 py-2">Poster session</td>
             <td className="px-4 py-2">Coffee Break & Poster session</td>
           </tr>
-          <tr className="border-t">
+          <tr className="border-t bg-gray-50">
             <td className="px-4 py-2 whitespace-nowrap">16:05–16:30</td>
             <td className="px-4 py-2">Invited talk (Di Guo)</td>
-            <td className="px-4 py-2">
-              The talk is focused on the embodied AI for human-robot interaction
-            </td>
+            <td className="px-4 py-2"> TBD</td>
           </tr>
-          <tr className="border-t bg-gray-50">
+          <tr className="border-t">
             <td className="px-4 py-2 whitespace-nowrap">16:30–16:55</td>
             <td className="px-4 py-2">Invited talk (Konstantin Yakovlev)</td>
             <td className="px-4 py-2">
-              The talk is focused on decentralized lifelong multi-agent pathfinding via learning
-            </td>
-          </tr>
-          <tr className="border-t">
-            <td className="px-4 py-2 whitespace-nowrap">16:55–17:20</td>
-            <td className="px-4 py-2">Invited talk (Boyu Zhou)</td>
-            <td className="px-4 py-2">
-              The talk is focused on the active perception of robot swarms
+              Multi-agent Path Finding: Heuristic Search Meets Machine Learning
             </td>
           </tr>
           <tr className="border-t bg-gray-50">
+            <td className="px-4 py-2 whitespace-nowrap">16:55–17:20</td>
+            <td className="px-4 py-2">Invited talk (Boyu Zhou)</td>
+            <td className="px-4 py-2">
+              Active Perception with Autonomous Aerial Robots
+            </td>
+          </tr>
+          <tr className="border-t">
             <td className="px-4 py-2 whitespace-nowrap">17:20–17:35</td>
             <td className="px-4 py-2">Challenge Awards</td>
             <td className="px-4 py-2">—</td>
           </tr>
-          <tr className="border-t">
+          <tr className="border-t bg-gray-50">
             <td className="px-4 py-2 whitespace-nowrap">17:35–18:00</td>
             <td className="px-4 py-2">Invited talk (Volker Strobel)</td>
-            <td className="px-4 py-2">
-              The talk is focused on the self-organizing systems for robot swarms
-            </td>
+            <td className="px-4 py-2">TBD</td>
           </tr>
         </tbody>
       </table>
