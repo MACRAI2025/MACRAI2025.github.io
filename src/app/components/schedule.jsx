27ae176d2977fc33b7e930e9a04e25681798e285
wenzhejia <1547321945@qq.com>
2025-10-16 10:47:04 +0800

    11

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -12,38 +12,38 @@ const Schedule = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-t">
+          {/* <tr className="border-t">
             <td className="px-4 py-2 whitespace-nowrap">14:00–14:10</td>
             <td className="px-4 py-2">Introduction and Opening Remarks</td>
             <td className="px-4 py-2">—</td>
-          </tr>
+          </tr> */}
 
           <tr className="border-t">
-            <td className="px-4 py-2 whitespace-nowrap">14:10–14:30</td>
+            <td className="px-4 py-2 whitespace-nowrap">14:00–14:20</td>
             <td className="px-4 py-2">Invited talk (Guido de Croon)</td>
             <td className="px-4 py-2">Swarms of tiny drones: from simulation to reality</td>
           </tr>
 
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">14:30–14:50</td>
+            <td className="px-4 py-2 whitespace-nowrap">14:20–14:40</td>
             <td className="px-4 py-2">Invited talk (Javier Alonso-Mora)</td>
             <td className="px-4 py-2">Learning-Driven Approaches to Motion Planning, Multi-Robot Task Allocation, and Collaborative Aerial Manipulation</td>
           </tr>
 
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">14:50–15:10</td>
+            <td className="px-4 py-2 whitespace-nowrap">14:40–15:00</td>
             <td className="px-4 py-2">Invited talk (Di Guo)</td>
             <td className="px-4 py-2">LLM-powered Multi-agent System and Proactive Collaboration</td>
           </tr>
 
           <tr className="border-t">
-            <td className="px-4 py-2 whitespace-nowrap">15:10–15:50</td>
+            <td className="px-4 py-2 whitespace-nowrap">15:00–15:40</td>
             <td className="px-4 py-2">Poster session</td>
             <td className="px-4 py-2">Coffee Break & Poster session</td>
           </tr>
 
           <tr className="border-t">
-            <td className="px-4 py-2 whitespace-nowrap">15:50–16:10</td>
+            <td className="px-4 py-2 whitespace-nowrap">15:40–16:00</td>
             <td className="px-4 py-2">Invited talk (Konstantin Yakovlev)</td>
             <td className="px-4 py-2">
               Multi-agent Path Finding: Heuristic Search Meets Machine Learning
@@ -51,7 +51,7 @@ const Schedule = () => {
           </tr>
 
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">16:10–16:30</td>
+            <td className="px-4 py-2 whitespace-nowrap">16:00–16:20</td>
             <td className="px-4 py-2">Invited talk (Boyu Zhou)</td>
             <td className="px-4 py-2">
               Active Perception with Autonomous Aerial Robots
@@ -59,30 +59,28 @@ const Schedule = () => {
           </tr>
 
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">16:30–16:50</td>
+            <td className="px-4 py-2 whitespace-nowrap">16:20–16:40</td>
             <td className="px-4 py-2">Invited talk (Siqi Zhou)</td>
             <td className="px-4 py-2">Aerial Swarm: From Methods to Tools Enabling Safe Coordination and Language-Based Interaction</td>
           </tr>
 
-          <tr className="border-t">
-            <td className="px-4 py-2 whitespace-nowrap">16:50–17:10</td>
-            <td className="px-4 py-2">Challenge Awards</td>
-            <td className="px-4 py-2">—</td>
-          </tr>
-
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">17:10–17:30</td>
+            <td className="px-4 py-2 whitespace-nowrap">16:40–17:00</td>
             <td className="px-4 py-2">Invited talk (Volker Strobel)</td>
             <td className="px-4 py-2">How foundation models will revolutionize robot swarms</td>
           </tr>
 
           <tr className="border-t bg-gray-50">
-            <td className="px-4 py-2 whitespace-nowrap">17:30–17:50</td>
+            <td className="px-4 py-2 whitespace-nowrap">17:00–17:20</td>
             <td className="px-4 py-2">Invited talk (Cao Yuhong)</td>
             <td className="px-4 py-2">High-Dimensional Multi-Agent Robot Learning</td>
           </tr>
 
-
+          <tr className="border-t">
+            <td className="px-4 py-2 whitespace-nowrap">17:20–17:40</td>
+            <td className="px-4 py-2">Awards Session</td>
+            <td className="px-4 py-2">—</td>
+          </tr>
 
         </tbody>
       </table>
